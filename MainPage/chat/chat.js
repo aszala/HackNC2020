089f8d8f357e6aac0827d041b5761fd52184059f
Abhay Zala <zala.abhay@gmail.com>
2020-10-17 23:08:34 -0400

    Fixed chat error. Allowed for friend picking in Main page

--- MainPage/chat/chat.js
+++ MainPage/chat/chat.js
@@ -11,32 +11,31 @@ Talk.ready.then(function () {
 					let userData = doc.data();
 					let otherData = otherDoc.data();
 
-						storage.ref(userData.profilePic).getDownloadURL().then((url) => {
-							storage.ref(otherData.profilePic).getDownloadURL().then((urlOther) => {
-								var me = new Talk.User({
-									id: auth.currentUser.uid,
-									name: userData.name,
-									email: userData.email,
-									photoUrl: url
-								});
-								var other = new Talk.User({
-									id: urlParams.get("id"),
-									name: otherData.name,
-									email: otherData.email,
-									photoUrl: urlOther
-								});
-								window.talkSession = new Talk.Session({
-									appId: "tPPos2DI",
-									me: me
-								});
-
-								var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other))
-								conversation.setParticipant(me);
-								conversation.setParticipant(other);
-
-								var inbox = talkSession.createChatbox(conversation);
-								inbox.mount(document.getElementById("talkjs-container"));
+					storage.ref(userData.profilePic).getDownloadURL().then((url) => {
+						storage.ref(otherData.profilePic).getDownloadURL().then((urlOther) => {
+							var me = new Talk.User({
+								id: auth.currentUser.uid,
+								name: userData.name,
+								email: userData.email,
+								photoUrl: url
 							});
+							var other = new Talk.User({
+								id: urlParams.get("id"),
+								name: otherData.name,
+								email: otherData.email,
+								photoUrl: urlOther
+							});
+							window.talkSession = new Talk.Session({
+								appId: "tPPos2DI",
+								me: me
+							});
+
+							var conversation = talkSession.getOrCreateConversation(Talk.oneOnOneId(me, other))
+							conversation.setParticipant(me);
+							conversation.setParticipant(other);
+
+							var inbox = talkSession.createChatbox(conversation);
+							inbox.mount(document.getElementById("talkjs-container"));
 						});
 					});
 				});
--- MainPage/main.js
+++ MainPage/main.js
@@ -5,30 +5,39 @@ function search() {
 		db.collection("users").get().then((snap) => {
 			snap.forEach((doc) => {
 				let data = doc.data();
-				if (data.name && data.name.trim().toLowerCase().includes(query.trim().toLowerCase())) {
-					storage.ref(data.profilePic).getDownloadURL().then((url) => {
-						let elements = `
-						<div class='search-result secondary-background fade'>
-							<img class='search-result-profilePic' src="${url}" >
-							<div class='search-result-data white'>
-								<h3 class='search-result-name'>${data.name}</h3>
-								<button class='connect-button' onclick='makePeer('${data.uid}')'>Make Peer</button>
-							</div>
-						</div>`;
-		
-						$("#search-results-name").append(elements);
-					});
+				if (data.uid !== auth.currentUser.uid) {
+					if (data.name && data.name.trim().toLowerCase().includes(query.trim().toLowerCase())) {
+						storage.ref(data.profilePic).getDownloadURL().then((url) => {
+							let elements = `
+							<div class='search-result secondary-background fade'>
+								<img class='search-result-profilePic' src="${url}" >
+								<div class='search-result-data white'>
+									<h3 class='search-result-name'>${data.name}</h3>
+									<button class='connect-button' onclick='makePeer('${data.uid}')'>Make Peer</button>
+								</div>
+							</div>`;
+
+							$("#search-results-name").append(elements);
+						});
+					}
 				}
 			});
 		});
 	}
 }
 
+function makePeer(uid) {
+	$("#" + uid).remove();
+	db.collection("users").doc(auth.currentUser.uid).update({
+		friends: firebase.firestore.FieldValue.arrayUnion(uid)
+	});
+}
+
 auth.onAuthStateChanged((user) => {
 	if (!user) {
 		document.location.replace("/login.html");
 	} else {
-		db.collection("users").doc(auth.currentUser.uid).get().then((doc) => {
+		db.collection("users").doc(auth.currentUser.uid).onSnapshot((doc) => {
 			let data = doc.data();
 			let friends = data.friends;
 
@@ -55,33 +64,35 @@ auth.onAuthStateChanged((user) => {
 				snap.forEach((docOther) => {
 					let dataOther = docOther.data();
 
-					let commonTags = "";
-					let count = 0;
-					for (let i=0;i<data.tags.length;i++) {
-						if (dataOther.tags.includes(data.tags[i])) {
-							commonTags += `${data.tags[i]}, `;
-							count++;
-							if (count == 5) {
-								break;
+					if (dataOther.uid !== auth.currentUser.uid && !(data.friends.join(',').includes(dataOther.uid))) {
+						let commonTags = "";
+						let count = 0;
+						for (let i=0;i<data.tags.length;i++) {
+							if (dataOther.tags.includes(data.tags[i])) {
+								commonTags += `${data.tags[i]}, `;
+								count++;
+								if (count == 5) {
+									break;
+								}
 							}
 						}
-					}
-					commonTags = commonTags.substring(0, commonTags.length - 2)
+						commonTags = commonTags.substring(0, commonTags.length - 2)
 
-					storage.ref(dataOther.profilePic).getDownloadURL().then((url) => {
-						let elements = `
-						<div class='search-result primary-background fade'>
-							<img class='search-result-profilePic' src="${url}" >
-							<div class='search-result-data white'>
-								<h3 class='search-result-name'>${dataOther.name}</h3>
-								<p>
-									Similar Tags: ${commonTags}
-								</p>
-								<button class='connect-button' onclick='makePeer('${dataOther.uid}')'>Make Peer</button>
-							</div>
-						</div>`;
-						$("#similar-tags").append(elements);
-					});
+						storage.ref(dataOther.profilePic).getDownloadURL().then((url) => {
+							let elements = `
+							<div class='search-result primary-background fade' id="${dataOther.uid}">
+								<img class='search-result-profilePic' src="${url}" >
+								<div class='search-result-data white'>
+									<h3 class='search-result-name'>${dataOther.name}</h3>
+									<p>
+										Similar Tags: ${commonTags}
+									</p>
+									<button class='connect-button' onclick='makePeer("${dataOther.uid}")'>Make Peer</button>
+								</div>
+							</div>`;
+							$("#similar-tags").append(elements);
+						});
+					}
 				});
 			});
 		});
